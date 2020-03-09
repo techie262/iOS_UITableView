daa2ef9981b21a584457df99b004419407a0a22b
dungdt30 <dungdt30@fsoft.com.vn>
2020-03-09 15:49:25 +0700

    create project

---
+++ Techie_iOS_UITableview/Techie_iOS_UITableview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Techie_iOS_UITableview
+//
+//  Created by MrButChi on 3/9/20.
+//  Copyright © 2020 techie262. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
